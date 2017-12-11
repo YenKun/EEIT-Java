d1f172eeb9d5c3fecbec1af618a2218a4194c2ad
YenKun <elvis100306070@gmail.com>
2017-12-11 10:15:28 +0800

    201712111015

--- JS_offClass/HwEx2/form.js
+++ JS_offClass/HwEx2/form.js
@@ -1,5 +1,9 @@
 var errormsg = "<i class='fa fa-times-circle'style='color:red;margin-left:10px'>錯誤格式</i>";
-var correctmsg = "<i class='fa fa-check-circle'style='color:green;margin-left:10px'>成功</i>"
+var correctmsg = "<i class='fa fa-check-circle'style='color:green;margin-left:10px'>成功</i>";
+document.getElementById("name").addEventListener("focus",
+  function() {
+    document.getElementById("warning1").innerHTML = "";
+  });
 document.getElementById("name").addEventListener("blur",
   function() {
     var name = document.getElementById("name").value;
@@ -16,27 +20,31 @@ document.getElementById("name").addEventListener("blur",
       }
     }
   });
+document.getElementById("pwd").addEventListener("focus",
+  function() {
+    document.getElementById("warning2").innerHTML = "";
+  });
 document.getElementById("pwd").addEventListener("blur",
   function() {
     var pwd = document.getElementById("pwd").value;
-    if(pwd.length<6){
-      document.getElementById("warning2").innerHTML = errormsg;
-    }else{
-      var reg = /^(?=.*\d)(?=.*[a-zA-Z])(?=.*\W).{6,10}$/;
-      if(reg.test(pwd)){
+      var reg = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*\W).{6,}$/;
+      if (reg.test(pwd)) {
         document.getElementById("warning2").innerHTML = correctmsg;
-      }else{
+      } else {
         document.getElementById("warning2").innerHTML = errormsg;
       }
+  });
+document.getElementById("date").addEventListener("focus",
+  function() {
+    document.getElementById("warning3").innerHTML = "";
+  });
+document.getElementById("date").addEventListener("blur",
+  function() {
+    var reg = /^\d{4}\/\d{2}\/\d{2}$/;
+    var date = document.getElementById("date").value;
+    if (reg.test(date)) {
+      document.getElementById("warning3").innerHTML = correctmsg;
+    } else {
+      document.getElementById("warning3").innerHTML = errormsg;
     }
   });
- document.getElementById("date").addEventListener("blur",
- function(){
-   var reg = /^\d{4}\/\d{2}\/\d{2}$/;
-   var date = document.getElementById("date").value;
-   if(reg.test(date)){
-     document.getElementById("warning3").innerHTML = correctmsg;
-   }else{
-     document.getElementById("warning3").innerHTML = errormsg;
-   }
- });
